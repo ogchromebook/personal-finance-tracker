fd9a00fe37ba00733164a919ebc7bd97a691e297
Sam Kachergius <156481065+ogchromebook@users.noreply.github.com>
2024-06-10 20:45:05 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const sequelize = require('../config/config');
-const User = require('./user');
-const Transaction = require('./transaction');
+const User = require('./User');
+const Transaction = require('./Transaction');
 
 User.hasMany(Transaction, { foreignKey: 'user_id' });
 Transaction.belongsTo(User, { foreignKey: 'user_id' });
