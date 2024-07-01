608c09c203a2d42eed5ef1df698ca631253128b3
Sam Kachergius <skachergius@gmail.com>
2024-07-01 18:00:35 -0500

    updated config file

--- config/config.js
+++ config/config.js
@@ -1,14 +1,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-  }
-);
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'postgres',
+    }
+  );
+}
 
 module.exports = sequelize;
